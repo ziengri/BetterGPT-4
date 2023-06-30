f05290af45415bbceab9fbc33446142a4fe2e728
Yvan <yvan.lokhmotov@icloud.com>
2023-06-30 22:49:16 +0100

    Added referral links.

--- src/components/Chat/ChatContent/ChatContent.tsx
+++ src/components/Chat/ChatContent/ChatContent.tsx
@@ -113,7 +113,38 @@ const ChatContent = () => {
               </div>
             )}
           </div>
-          <div className='w-full h-36'></div>
+          <div className='w-full h-36'>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '12vh',
+              }}
+            >
+              <a href='https://publishers.adsterra.com/referral/68k22Zpr86'>
+                <img
+                  alt='banner'
+                  src='https://landings-cdn.adsterratech.com/referralBanners/gif/720x90_adsterra_reff.gif'
+                />
+              </a>
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '15vh',
+              }}
+            >
+              <a href='https://dashboard.capsolver.com/passport/register?inviteCode=Ecv6Gtrh0ECa'>
+                <img
+                  alt='banner'
+                  src='https://camo.githubusercontent.com/359a6866e8e9700c4cfa18e3bea8055e886772c93bc17618ea67ecfae8ca0d0d/68747470733a2f2f63646e2e646973636f72646170702e636f6d2f6174746163686d656e74732f313130353137323339343635353632353330362f313130353138303130313830323437313537352f32303232313230372d3136303734392e676966'
+                />
+              </a>
+            </div>
+          </div>
         </div>
       </ScrollToBottom>
     </div>
